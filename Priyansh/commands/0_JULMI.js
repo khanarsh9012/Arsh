3b9bc335a055d4e12e60ce37f4ae9f10f46878e0
khanarsh9012 <khanarsh901285@gmail.com>
2025-08-19 16:16:40 +0530

    Update 0_JULMI.js

--- Priyansh/commands/0_JULMI.js
+++ Priyansh/commands/0_JULMI.js
@@ -21,7 +21,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("Admin")) {
 		var msg = {
 				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖जमाल≛पठान❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://www.instagram.com/alex972740?igsh=MWNpbDFnMzYyM3p5Nw==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://i.ibb.co/DD84WBF9/813902034295613.jpg  ",
-				attachment: fs.createReadStream(__dirname + `/noprefix/Julmi.jpg`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/Jamal.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
