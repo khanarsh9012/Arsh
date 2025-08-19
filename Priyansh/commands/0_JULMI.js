f5dbf6bc6243c77ffb201bac8bea141f1070bdc2
khanarsh9012 <khanarsh901285@gmail.com>
2025-08-19 15:56:16 +0530

    Update 0_JULMI.js

--- Priyansh/commands/0_JULMI.js
+++ Priyansh/commands/0_JULMI.js
@@ -1,6 +1,6 @@
- const fs = require("fs");
+const fs = require("fs");
 module.exports.config = {
-	name: "Julmi",
+	name: "Jamal",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "Arun", 
@@ -13,15 +13,15 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("जुल्मी जाट") ||
+	if(react.includes("जमाल पठान") ||
      react.includes("Boss") || 
      react.includes("BOSS") || 
      react.includes("ADMIN") ||
      react.includes("admin") || 
 react.includes("Admin")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖जुल्मी≛जाट❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/Julmi.jpg`)
+				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖जमाल≛पठान❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://www.instagram.com/alex972740?igsh=MWNpbDFnMzYyM3p5Nw==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://www.facebook.com/share/1CA5AgUBLN///`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/ module.exports.config`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
@@ -29,4 +29,4 @@ react.includes("Admin")) {
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}
