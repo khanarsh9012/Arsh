3a6e09fed192ab27e9743a9189f1a3c7e2883718
khanarsh9012 <khanarsh901285@gmail.com>
2025-08-19 23:15:55 +0530

    Update war.js

--- Priyansh/commands/war.js
+++ Priyansh/commands/war.js
@@ -20,32 +20,32 @@ module.exports.run = async function({ api, args, Users, event}) {
     var arraytag = [];
         arraytag.push({id: mention});
     var a = function (a) { api.sendMessage(a, event.threadID); }
-a("Listen to your father, kids !");
-setTimeout(() => {a({body: "F*ck your mother" })}, 3000);
-setTimeout(() => {a({body: "You little brats come out to listen to your father curse"})}, 5000);
-setTimeout(() => {a({body: "Quick show the dogs" })}, 7000);
-setTimeout(() => {a({body: "Show your father's soul" })}, 9000);
-setTimeout(() => {a({body: "Do you guys like war so much?" })}, 12000);
-setTimeout(() => {a({body: "Damn you guys too" })}, 15000);
-setTimeout(() => {a({body: "Give your father the age of war" })}, 17000);
-setTimeout(() => {a({body: "Hurry up and curse each other with me" })}, 20000);
-setTimeout(() => {a({body: "Are the bad boys wrinkling their noses up to wage war on your father?" })}, 23000);
-setTimeout(() => {a({body: "I fuck your mother" })}, 25000);
-setTimeout(() => {a({body: "Delicious then yawn your mother up" })}, 28500);
-setTimeout(() => {a({body: "Your father shot you to death by rapping" })}, 31000);
-setTimeout(() => {a({body: "Please age eat me ?" })}, 36000);
-setTimeout(() => {a({body: "If it's delicious, eat your dad" })}, 39000);
-setTimeout(() => {a({body: "Before that, please give me a break for 1 minute" })}, 40000);
-setTimeout(() => {a({body: "Please allow me to start" })}, 65000);
-setTimeout(() => {a({body: "First of all, I would like to fuck you from top to bottom" })}, 70000);
-setTimeout(() => {a({body: "I fuck from cunt hole to pussy cleavage" })}, 75000);
-setTimeout(() => {a({body: "The cunt is as big as a buffalo's cunt masturbating a sewer pipe" })}, 80000);
-setTimeout(() => {a({body: "I'm sure 2 guys like me aren't enough to fill your ass hole" })}, 85000);
-setTimeout(() => {a("I'm tired and don't curse anymore")} , 90000);
-setTimeout(() => {a({body: "Come on boss update the lyric, let's continue the war" })}, 95000);
-setTimeout(() => {a({body: "Thank you for listening to me war" })}, 100000);
-setTimeout(() => {a({body: "Goodbye and see you in the next program" })}, 105000);
-setTimeout(() => {a({body: "Good bye 🥺"})} , 115000);
+a("RANDI MAKE BACHA TERI BHEN,KO CHODU!");
+setTimeout(() => {a({body: "TERI BHEN KI CHUT MARU RANDI KE BACHA" })}, 3000);
+setTimeout(() => {a({body: "TERI AMMA KO CHODU RANDI BANAKE MAXRCHOD"})}, 5000);
+setTimeout(() => {a({body: "TERI BAJI KJ CHUT PE CHAPAL MARU" })}, 7000);
+setTimeout(() => {a({body: "TERI BHEN KO UTHA UTHA KE PATAK PATAK KE CHODU TAPK GANDU TATA" })}, 9000);
+setTimeout(() => {a({body: "TERI MOSI KI CHUT PE CHAPAL MARU GANDU TAPK HARM KE PILE" })}, 12000);
+setTimeout(() => {a({body: "SALE AUKAT BANA BAPP KE SAMNA HAWABJI KREGA BHENCHOD" })}, 15000);
+setTimeout(() => {a({body: "TERI MOM KI SANDA KA TEL LAGA KE CHODU KUTTE KE BACHA" })}, 17000);
+setTimeout(() => {a({body: "TU SALE BAPP KI TYP KO CP BOLEGA GANDU" })}, 20000);
+setTimeout(() => {a({body: "TERI AMMA KO TADPA TADPA KE CHODUGA WAIT KAR BETA" })}, 23000);
+setTimeout(() => {a({body: "TERI PURE KHANDAN KO CHODUGA BAPP SE FYT KREGA TU BALAK" })}, 25000);
+setTimeout(() => {a({body: "TAPKA GANDU BHEN KE LUND KHA BHAGTA HA RANDI GASTI KE BACHA" })}, 28500);
+setTimeout(() => {a({body: "TU APNA JIJA JAMAL KO FEEL KAR RO MAT AJA LOL KIDS" })}, 31000);
+setTimeout(() => {a({body: "TUJHA LEGAND FYTER BANATA HU TAPK SALE SWAR KE BACHA TERI MADA FUDDA" })}, 36000);
+setTimeout(() => {a({body: "TERI BAJI KO BINA CONDOM KE CHODU GANDU NASAL KE BACHA " })}, 39000);
+setTimeout(() => {a({body: "TU KAREGA APNE BAPP SE FIGHT TERI AMMI CHOD KE MERA LUND HOGYA TIGHT" })}, 40000);
+setTimeout(() => {a({body: "TU BINA CONDOM KI OLAD HA MERI JA APNI MAA SE PHUCHNA TUJHA KISKE LUND SE BHAR AYA" })}, 65000);
+setTimeout(() => {a({body: "TUJHA MA BANUGA FIGHTER KI JHAT BAS AB TU TYP KARTA JA DALLE MAKE BACHA GAREEB KIDS" })}, 70000);
+setTimeout(() => {a({body: "CHAL BETA AJA TERI BHEN KO GB ROAD PE LE JAKE CHODU" })}, 75000);
+setTimeout(() => {a({body: "SALE FAKIR KI OLAD BHIKARI TATA TU CP MAREGA MA TERI GAND MARUGA" })}, 80000);
+setTimeout(() => {a({body: "TERI BHEN KOO CONDOM BINA CHODUGA TU SABAR KAR TUJHA USKI TADAP SUNYI DEGI GANDU KI OLAD" })}, 85000);
+setTimeout(() => {a("")}:"TERI BHEN KE BUR PE CHAPAL MARKE  CHODUGA SALA GWAR KIDS TATA OKAT BNA APNI" 90000);
+setTimeout(() => {a({body: "SALE JAHIL BAPP KI OLAD RO MAT AB TERA BAPP JAMAL TUM SAB TATAO KO AKELA CHODUGA KHULE MEDAN MAI" })}, 95000);
+setTimeout(() => {a({body: "TEERI RANDI MASE PHUCH TU KIDS CHUDWA MAA KA BACHA SALA JHATU GWAER BHIKARI TATA" })}, 100000);
+setTimeout(() => {a({body: "TERI MA BHEN KO BURZKHALIFA PAR BETHA KAR CHODUGA TU PURI ZINDAGI KAREGA GGAREEBREEB MADRCHOD" })}, 105000);
+setTimeout(() => {a({body: "TUNE BAPP SE PANGA LIYA HA OR YE TERA BAPP AB TUJHKO NANGA KREGA JHATU FAKIR MADRCHOD KIDS KE BACHA "})} , 115000);
 
 
 
