182a0453dba7d345eb585a0c9205d00f8c77c809
khanarsh9012 <khanarsh901285@gmail.com>
2025-08-20 02:30:58 +0530

    Update 0_JULMI.js

--- Priyansh/commands/0_JULMI.js
+++ Priyansh/commands/0_JULMI.js
@@ -13,14 +13,15 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("Jamal") ||
+	if(react.includes("जमाल पठान") ||
      react.includes("Boss") || 
      react.includes("BOSS") || 
      react.includes("ADMIN") ||
      react.includes("admin") || 
 react.includes("Admin")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖जमाल❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://www.instagram.com/alex972740?igsh=MWNpbDFnMzYyM3p5Nw==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://i.ibb.co/DD84WBF9/813902034295613.jpg/",
+				body: "★𝗢𝘄𝗻𝗲𝗿ﮩ٨ـﮩ💚💖ـ٨\n\n✦🌸===『*★🌸◉❖जमाल≛पठान❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://www.instagram.com/alex972740?igsh=MWNpbDFnMzYyM3p5Nw==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://www.facebook.com/share/1J69hApStW//`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/julmi (2).jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
@@ -28,4 +29,4 @@ react.includes("Admin")) {
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-	}
+	   }
